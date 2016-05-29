fafb491556ce6a2bc7fc31a2e22bfa49b5496399
yamBean <fan.w_2005@163.com>
2016-05-30 00:11:43 +0800

    there is no if-else in jsx

--- project_source/getting_started/04_editing_notes/kanban-app/app/NoteEditable.jsx
+++ project_source/getting_started/04_editing_notes/kanban-app/app/NoteEditable.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 export default ({task, onFinish, onDelete}) => (
   <div>
-    <input className="note-input" onBlur={onFinish} defaultValue={task} autoFocus={true} readOnly={false} />
+    <input className="note-input" onBlur={onFinish} defaultValue={task} autoFocus={true} readOnly={false}
+       ref={ (e) => e ? e.selectionStart = 0/*this.props.task.length*/ : null }
+    />
     <button onClick={onDelete}>x</button>
   </div>
 );
--- project_source/getting_started/04_editing_notes/kanban-app/app/Notes.jsx
+++ project_source/getting_started/04_editing_notes/kanban-app/app/Notes.jsx
@@ -7,12 +7,16 @@ export default ({ notes, onEdit=()=>{}, onDelete=()=>{}, onFinish=()=>{} }) => {
   return (
     <ul className="note-ul">{notes.map(({id, task, editable}) =>
       <li key={id}>
-          {editable?
-              <NoteEditable onFinish={onFinish.bind(null, id)} onDelete={onDelete.bind(null, id)} task={task} />
-              :
-              <Note onEdit={onEdit.bind(null, id)} onDelete={onDelete.bind(null, id)} task={task} />
-          }
+          {generateNote(id, task, editable, onEdit, onDelete, onFinish)}
       </li>
     )}</ul>
   );
+
+}
+
+var generateNote = (id, task, editable, onEdit=()=>{}, onDelete=()=>{}, onFinish=()=>{}) => {
+    if(editable){
+        return <NoteEditable onFinish={onFinish.bind(null, id)} onDelete={onDelete.bind(null, id)} task={task} />
+    }
+    return <Note onEdit={onEdit.bind(null, id)} onDelete={onDelete.bind(null, id)} task={task} />
 }
