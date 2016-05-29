20a9be5d70b02371a19059fbdce03916f6d3b0c9
yamBean <fan.w_2005@163.com>
2016-05-29 23:00:22 +0800

    study onDoubleClick

--- project_source/getting_started/04_editing_notes/kanban-app/app/Note.jsx
+++ project_source/getting_started/04_editing_notes/kanban-app/app/Note.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default ({task, onEdit, onDelete}) => (
   <div>
-    <span onClick={onEdit}>{task}</span>
+    <span onDoubleClick={onEdit}>{task}</span>
     <button onClick={onDelete}>x</button>
   </div>
 );
